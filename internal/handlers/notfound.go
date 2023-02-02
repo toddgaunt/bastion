ca0a34af2da50944c72a0a4d91d3b48789d33a1b
Todd Gaunt <todd@bastionburrow.com>
2023-02-01 21:42:27 -0500

    Return 404 Not Found for non-existent pages

--- internal/handlers/notfound.go
+++ internal/handlers/notfound.go
@@ -6,7 +6,7 @@ import (
 	"github.com/toddgaunt/bastion/internal/errors"
 )
 
-var errNotFound = errors.Annotation{WithStatus: http.StatusBadRequest}.Wrap(errors.New("not found"))
+var errNotFound = errors.Annotation{WithStatus: http.StatusNotFound}.Wrap(errors.New("not found"))
 
 func (env Env) NotFound(w http.ResponseWriter, r *http.Request) {
 	handleError(w, errNotFound, env.Logger)
